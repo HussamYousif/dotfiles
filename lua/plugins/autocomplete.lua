cdaef2f48569523a42d8eaa46af5e614ae577cc8
HussamYousif <hussam_samir@hotmail.com>
2026-03-02 13:53:29 +0100

    Integrates `blink.cmp` with `LuaSnip` for snippets
    Activates `blink.cmp` as the primary autocompletion and snippet provider.
    Configures `LuaSnip` as the snippet engine, loading snippets from `friendly-snippets` and a dedicated user snippets directory.
    Adds a comprehensive set of Clojure-specific snippets, enhancing development experience for Clojure projects.

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,8 +1,37 @@
 return {
   { "hrsh7th/nvim-cmp", enabled = false },
-  { "saghen/blink.cmp", enabled = false },
-  { "L3MON4D3/LuaSnip", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  -- Optionally disable mini.snippets if used
+  {
+    "saghen/blink.cmp",
+    enabled = true,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+        end,
+      },
+    },
+    opts = {
+      snippets = { preset = "luasnip" },
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      },
+      sources = {
+        default = { "snippets", "lsp" },
+        providers = {
+          snippets = {
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+  { "rafamadriz/friendly-snippets", enabled = true },
   { "nvim-mini/mini.snippets", enabled = false },
 }
