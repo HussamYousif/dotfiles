a8fcf5f226756289021e71af8bc90ec9d15734c9
HussamYousif <hussam_samir@hotmail.com>
2026-04-10 08:27:45 +0200

    adds snippets

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -10,27 +10,27 @@ return {
         version = "v2.*",
         build = "make install_jsregexp",
         config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
           require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
         end,
       },
     },
-    opts = {
-      snippets = { preset = "luasnip" },
-      keymap = {
+    opts = function(_, opts)
+      opts.snippets = { preset = "luasnip" }
+      opts.keymap = {
         preset = "default",
         ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      },
-      sources = {
-        default = { "snippets", "lsp" },
+      }
+      opts.sources = {
+        default = { "lsp", "snippets" },
         providers = {
           snippets = {
             score_offset = 100,
           },
         },
-      },
-    },
+      }
+      return opts
+    end,
   },
   { "rafamadriz/friendly-snippets", enabled = true },
   { "nvim-mini/mini.snippets", enabled = false },
